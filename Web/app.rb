d0be9eed9b2b7b5b233be0356452a8da414925d9
Sidmarek <chodec.marek@gmail.com>
2014-08-06 23:11:05 +0200

    Script for printing data from the  previous form

--- Web/app.rb
+++ Web/app.rb
@@ -1,8 +1,11 @@
 require "rubygems"
 require "sinatra"
 get '/hello/:name/:city/' do
-"Hello #{params[:name]} from #{params[:city]}!"
+ "Hello #{params[:name]} from #{params[:city]}!"
 end
-get '/hello/:name/' do
-params[:name]
+get '/form' do
+ erb :secret
+end
+post '/form' do
+ "Thats yours data:<br>'#{params[:name]}'<br>'#{params[:username]}'<br> Here is length of your password:<br>#{params[:password].length}"
 end
